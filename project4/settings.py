42bc38344d78fe147f80b64aef45a5cf6885e06f
Kelly MacDevette <kellymacdev@gmail.com>
2025-11-21 16:26:47 +0200

    corrected allowed hosts with new domain name

--- project4/settings.py
+++ project4/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["twcyearbook.onrender.com", "127.0.0.1"]
-#ALLOWED_HOSTS = ["twcyearbook.onrender.com"]
-
+ALLOWED_HOSTS = ["yearbook-public.onrender.com", "127.0.0.1"]
 
 # Application definition
 
